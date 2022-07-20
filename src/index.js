24c069cf9d25669dac89c243613a9875cbd871ce
Claudio Puggioni <claudieddu@hotmail.com>
2022-07-20 05:37:41 -0700

    final until not final

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import UserInterface from "./components/UserInterface";
-import PendingCxt from "./contexts/PendingPurchase.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
